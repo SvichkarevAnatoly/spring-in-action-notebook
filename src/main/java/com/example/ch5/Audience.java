de281a398db930459a6f765c31616c5d5aa20150
SvichkarevAnatoly <svichkarev.anatoly@gmail.com>
2016-11-14 18:03:08 +0300

    другой вариант аспекта, выполняющего те же действия + замеряющий время работы, но из-за него не возвращается строка от performer

--- src/main/java/com/example/ch5/Audience.java
+++ src/main/java/com/example/ch5/Audience.java
@@ -1,5 +1,7 @@
 package com.example.ch5;
 
+import org.aspectj.lang.ProceedingJoinPoint;
+
 public class Audience {
     public void takeSeats() {
         System.out.println("The audience is taking their seats.");
@@ -15,5 +17,22 @@ public class Audience {
 
     public void demandRefund() {
         System.out.println("Boo! We want our money back!");
+    }
+
+    public void watchPerformance(ProceedingJoinPoint joinpoint) {
+        try {
+            takeSeats();
+            turnOffCellPhones();
+            long start = System.currentTimeMillis();
+
+            joinpoint.proceed();
+
+            long end = System.currentTimeMillis();
+            applaud();
+            System.out.println("The performance took " + (end - start)
+                    + " milliseconds.");
+        } catch (Throwable t) {
+            demandRefund();
+        }
     }
 }
